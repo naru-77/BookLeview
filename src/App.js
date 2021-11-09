aac10ca397a7e4feb3a5a7e88e5bcf11445a7926
naru-77x <naru.fumi@icloud.com>
2021-11-09 14:20:20 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from "react";
+import './App.css';
+import './css/Login.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Signup() {
+  return <h2>Signup</h2>;
+}
+
+function Login() {
+  return <main className="form-signin">
+  <form>
+    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div className="form-floating">
+      <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+      <label htmlFor="floatingInput">Email address</label>
+    </div>
+    <div className="form-floating">
+      <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+      <label htmlFor="floatingPassword">Password</label>
+    </div>
+
+    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+  </form>
+  </main>;
+}
